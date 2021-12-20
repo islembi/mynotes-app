045f57cf7a6ba068d04cac677cd2d02c5e68a4dc
hediismail <hedi.ismail.tr.1990@gmail.com>
2021-12-20 16:45:06 +0100

    ajout note + markdown + css

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,8 +1,22 @@
 import React from "react"
-export default function Notes({ carnet }) {
+import "bootstrap/dist/css/bootstrap.css"
+import ModalNotes from "./../ModalNotes/ModalNotes"
+import { Markdown } from "react-showdown"
+
+export default function Notes({ carnet, carnets}) {
   return (
     <div>
-      <button>{carnet.titre}</button>
+      <h2>{carnet.titre}</h2>
+      <ModalNotes carnet={carnet} carnets={carnets} />
+      {carnet.notes.map((note) => {
+        return (
+          <div key={note.id}>
+            <h3>{note.titre}</h3>
+            <h5>{note.categorie}</h5>
+            <Markdown className="markdown" markup={note.text} />
+          </div>
+        )
+      })}
     </div>
   )
 }
