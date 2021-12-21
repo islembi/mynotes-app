b339ea4824d6abef11d7e46720ab72e5a676d2f0
islem <islem.benothmen.27@gmail.com>
2021-12-21 09:25:21 +0100

    modification navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import Carnet from "../Carnet/Carnet"
 import ModalComp from "../Modal/ModalComp"
 import uuid from "react-uuid"
@@ -7,18 +7,19 @@ export default function Navbar() {
   const [titre, setTitre] = useState("")
   const [rech, setRech] = useState("")
   const [carnets, setCarnets] = useState(
-    localStorage.carnets ? JSON.parse(localStorage.carnets) : [])
+    localStorage.carnets ? JSON.parse(localStorage.carnets) : []
+  )
 
   useEffect(() => {
-    localStorage.setItem("carnets", JSON.stringify(carnets));
-  }, [carnets]);
+    localStorage.setItem("carnets", JSON.stringify(carnets))
+  }, [carnets])
 
   //ajout de carnet
   const ajout = (inputTitre) => {
     const tmp = [...carnets]
     tmp.push({ id: uuid(), titre: inputTitre, date: Date.now() })
     setCarnets(tmp)
-    setTitre("") 
+    setTitre("")
   }
 
   const modifierTitre = (id) => {
@@ -43,25 +44,28 @@ export default function Navbar() {
   let ligneCarnet = rechercher(rech, carnets).map((carnet) => {
     return (
       <div>
+        <div onClick={window["displayNotes"]}></div>
         <Carnet
           key={carnet.id}
-                carnet={carnet}
-                titre={titre}
-                setTitre={setTitre}
-                modifierTitre={modifierTitre}
-                deleteCarnet={deleteCarnet}
+          carnet={carnet}
+          titre={titre}
+          setTitre={setTitre}
+          modifierTitre={modifierTitre}
+          deleteCarnet={deleteCarnet}
         />
       </div>
     )
   })
 
-  function deleteCarnet(id){
-    let rep = window.confirm("vous voulez supprimer?" );
-    if(rep === false) return;
-    const tmp = carnets.filter(function(el) { return el.id != id; });
-    setCarnets(tmp);
-  } 
-  
+  function deleteCarnet(id) {
+    let rep = window.confirm("vous voulez supprimer?")
+    if (rep === false) return
+    const tmp = carnets.filter(function (el) {
+      return el.id != id
+    })
+    setCarnets(tmp)
+  }
+
   return (
     <div>
       <div className="sidebar-container">
@@ -86,13 +90,19 @@ export default function Navbar() {
 
           <div> {ligneCarnet}</div>
 
-
           <li></li>
         </ul>
       </div>
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand">My Notes ✏</a>
       </nav>
+      <div id="myNotes">
+        {" "}
+        <div className="container-notes">
+          {" "}
+          <Notes />{" "}
+        </div>{" "}
+      </div>
     </div>
   )
 }
---
+++ src/components/notes/notes.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+export default function Notes() {
+  return (
+    <div>
+      <button>Ajouter une note</button>
+    </div>
+  )
+}
